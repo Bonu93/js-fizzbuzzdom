8b583745eac0e1dbd5ad9c75387736a3d2f7ff16
Bonu93 <luca.morucci.kb@gmail.com>
2021-10-25 17:39:39 +0200

    complete

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5
 
 const container = document.querySelector('.container')
-console.log(container);
 
 for (let i = 1; i <= 99; i++) {
 
@@ -17,5 +16,5 @@ for (let i = 1; i <= 99; i++) {
     else {
         container.innerHTML += `<div class="box">${i}</div>`
     }
-    
+
 }
